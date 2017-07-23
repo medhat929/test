de7f4ec6807cc9e88b7e5b22f7d90ad577525f24
Ahmedsk <ahmedsk128@gmail.com>
2017-07-24 00:34:29 +0200

    some 3k

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php 
-/* ay 7aga */
-echo "this is our routes";
+{
 
-Route::get('any', 'routers@index');
+    /* ay 7aga */
+    echo "this is our routes";
 
-
-Route::get('Hi', function(){
-    echo "heloo";
-});
+    Route::get('any', 'routers@index');
 
 
+    Route::get('Hi', function(){
+        echo "helooo";
+    });
+
+    Route::post('/admin/{id}', 'adminController@pay');
+
+}
 ?>
